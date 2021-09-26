b458e2d4f7c7ad7b1b12ffe6dce8a68a8213fa2b
Bartek <bartekd001@gmail.com>
2021-09-26 16:36:12 +0200

    quiz

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    db: 'mongodb+srv://exampleDB:qBTiLf8cOdkOW4PR@cluster0.ycjaq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    db: '',
     keySession: ['Klucz'],
     maxAgeSession: 24 * 60 * 60 * 1000
 }
